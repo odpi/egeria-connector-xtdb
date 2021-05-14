da901cb3f98d0a2c1cdd12791b269dcfa288b85e
Christopher Grote <chris@thegrotes.net>
2021-05-14 18:52:00 +0100

    Correct bash loop
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

--- cts/charts/ec-ha-crux/bin/waitForStatefulSet.sh
+++ cts/charts/ec-ha-crux/bin/waitForStatefulSet.sh
@@ -8,7 +8,7 @@ REPLICA=0
 while [ $REPLICA -lt $NUM_REPLICAS ]; do
   ENDPOINT="https://${CRUX_RELEASE_NAME}-crux-${REPLICA}.${INTERNAL_SVC_NAME}:9443"
   echo " ... checking availability of: ${ENDPOINT}"
-  until [ $(curl -f -k -w "\n  (%{http_code} - %{url_effective})\n" --silent -X GET ${ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${CRUX_SERVER}/configuration) ]; do
+  until curl -f -k -w "\n  (%{http_code} - %{url_effective})\n" --silent -X GET ${ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${CRUX_SERVER}/configuration; do
     echo " ... waiting another 10 seconds for ${ENDPOINT} to be in a running state."
     sleep 10
   done
