25cf1f7be16f763d19f2428902d6474e7cadeeb4
Christopher Grote <chris@thegrotes.net>
2021-11-10 14:09:13 +0000

    Migrates testing scripts to new chart structure
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

--- cts/runAndCollect.sh
+++ cts/runAndCollect.sh
@@ -10,24 +10,23 @@ echo "Kicking off the $chart_name chart ($scale)..."
 helm install -f ${scale}.yaml "$scale" $chart_name
 
 echo "Capturing the names of the pods of interest..."
-pod_report=$(kubectl get pods | grep "${scale}"-init-and-report | awk '{ print $1 }')
+pod_report=$(kubectl get pod -l job-name=${scale}-report --no-headers -o name)
 echo " ... report pod: $pod_report"
-pod_actual=$(kubectl get pods | grep "${scale}"-platform | awk '{ print $1 }')
-echo " ... actual pod: $pod_actual"
 
 echo "Monitoring the reporting pod until we see completion..."
-report_status="None"
-while [[ "$report_status" != "-- End of conformance test suite results collection, download from: /tmp/$scale.tar.gz" && "$report_status" != "-- End of performance test suite results collection, download from: /tmp/$scale.tar.gz" ]]; do
+report_status="Starting"
+while [[ "$report_status" != "Running" ]]; do
   echo " ... found status $report_status -- waiting ..."
   sleep 120
-  report_status=$(kubectl logs --tail=1 "${pod_report}")
+  report_status=$(kubectl get pod -l job-name=${scale}-report --no-headers -o custom-columns=":status.phase")
 done
 
 echo "Detailing runtime and reports into ./$scale..."
 mkdir -p ./"$scale"
-kubectl cp "$pod_report":/tmp/"$scale".tar.gz ./"$scale"/"$scale".tar.gz
-kubectl describe pod "$scale" > ./"$scale"/deployment
-kubectl get configmap "$scale"-env -o yaml > ./"$scale"/configmap.yaml
+cd ./"$scale"
+kubectl exec "$pod_report" -- sh -c 'cat /export/pipe' | tar -xvf -
+kubectl describe pod "$scale" > ./deployment
+kubectl get configmap "$scale"-env -o yaml > ./configmap.yaml
 
 echo "Deleting completed helm chart..."
 helm delete "$scale"
